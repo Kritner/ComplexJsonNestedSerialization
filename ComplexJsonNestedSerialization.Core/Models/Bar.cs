88b6500e85a719ba86922f8c5c57e7be7752c299
Russ Hammett <kritner@gmail.com>
2018-02-19 09:46:18 -0500

    Adding property that should (eventually) control what properties of Baz are serialized

--- ComplexJsonNestedSerialization.Core/Models/Bar.cs
+++ ComplexJsonNestedSerialization.Core/Models/Bar.cs
@@ -5,6 +5,7 @@ namespace ComplexJsonNestedSerialization.Core.Models
     public class Bar
     {
         public int Id { get; set; }
+        public bool ShouldSerializeSomeBazProperty { get; set; }
         public IEnumerable<Baz> Bazez { get; set; }
     }
 }
