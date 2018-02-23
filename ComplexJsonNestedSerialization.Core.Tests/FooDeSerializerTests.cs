c8a5b9b5d108b5e222708e6ac58841257d276d5e
Russ Hammett <Kritner@gmail.com>
2018-02-23 13:11:44 -0500

    Renamed file as tests are relevant to both serialization and deserialization

--- ComplexJsonNestedSerialization.Core.Tests/FooDeSerializerTests.cs
+++ ComplexJsonNestedSerialization.Core.Tests/FooDeSerializerTests.cs
@@ -14,7 +14,7 @@ using Newtonsoft.Json;
 namespace ComplexJsonNestedSerialization.Core.Tests
 {
     [TestFixture]
-    public class FooSerializerTests
+    public class FooDeSerializerTests
     {
 
         private FooSerializer<Bar, Baz> _subject;
