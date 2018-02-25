5fa8f62c3e1b1e0f54cd38d9779f1c28c813b3b5
Russ Hammett <kritner@gmail.com>
2018-02-25 14:03:53 -0500

    Moving `BazConverters` to their owner classes.  Added `IBazConverter`

---
+++ ComplexJsonNestedSerialization.Core/Interfaces/IBarConverter.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComplexJsonNestedSerialization.Core.Interfaces
+{
+    public interface IBarConverter<TBar, TBaz>
+        where TBar : IBar<TBar, TBaz>
+        where TBaz : IBaz<TBar>
+    {
+        void WriteJson(JsonWriter writer, TBar bar, JsonSerializer serializer);
+    }
+}
--- ComplexJsonNestedSerialization.Core/JsonConverters/BazConverterClient.cs
+++ ComplexJsonNestedSerialization.Core/JsonConverters/BazConverterClient.cs
@@ -1,19 +1,8 @@
 ﻿using System.Reflection;
-using ComplexJsonNestedSerialization.Core.Interfaces;
 using ComplexJsonNestedSerialization.Core.Models;
 
 namespace ComplexJsonNestedSerialization.Core.JsonConverters
 {
-    /// <summary>
-    /// All properties included in server projection
-    /// </summary>
-    /// <typeparam name="TBaz">The type to serialize</typeparam>
-    public class BazConverterServer<TBar, TBaz> : BazConverterBase<TBar, TBaz>
-        where TBar : IBar<TBar, TBaz>
-        where TBaz : IBaz<TBar>
-    {
-    }
-
     /// <summary>
     /// Property omitted from projection (just using a random one as an example)
     /// </summary>
